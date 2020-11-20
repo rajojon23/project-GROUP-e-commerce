a31127c5ed31b48dd467baefef8891009c8ba142
marcospoto <marcospoto@hotmail.com>
2020-11-20 10:40:14 -0500

    backend

---
+++ server/data.js
@@ -0,0 +1,35 @@
+const fs = require("file-system");
+
+const companies = JSON.parse(fs.readFileSync("./data/companies.json"));
+
+const items = JSON.parse(fs.readFileSync("./data/items.json"));
+
+const updatedArr = [];
+
+const updatedItemArr = [];
+
+items.forEach((item) => {
+  updatedItemArr.push({
+    name: item.name,
+    price: item.price,
+    bodyLocation: item.body_location,
+    category: item.category,
+    id: item._id,
+    image: item.imageSrc,
+    numInStock: item.numInStock,
+    companyId: item.companyId,
+  });
+});
+
+companies.forEach((item) => {
+  updatedArr.push({
+    name: item.name,
+    url: item.url,
+    country: item.country,
+    id: item._id,
+  });
+});
+
+fs.writeFileSync("server/data/fixedCompanies.json", JSON.stringify(updatedArr));
+
+fs.writeFileSync("server/data/items.json", JSON.stringify(updatedItemArr));
