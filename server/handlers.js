0e98f81e939bb97f10be16e3abf241211719f212
tintha <tintha@gmail.com>
2020-11-20 10:45:28 -0500

    get endpoints

---
+++ server/handlers.js
@@ -0,0 +1,49 @@
+const { updatedItemArr, updatedArr } = require("./dataTest");
+
+// returns all the items
+const handleAllItems = (req, res) => {
+  if (updatedItemArr.length === 0) {
+    res.status(404).json({
+      status: 404,
+      message: "No data found",
+    });
+  } else {
+    res.status(200).json({
+      status: 200,
+      data: updatedItemArr,
+    });
+  }
+};
+
+// returns data about a single item by id
+const handleItemById = (req, res) => {
+  const { id } = req.params;
+  const searchedItem = updatedItemArr.find((item) => item.id === Number(id));
+  if (!searchedItem) {
+    res.status(404).json({
+      status: 404,
+      message: "Item not found",
+    });
+  } else {
+    res.status(200).json({
+      status: 200,
+      data: { searchedItem },
+    });
+  }
+};
+
+// returns the sellers
+const handleCompanies = (req, res) => {
+  res.status(200).json({
+    status: 200,
+    data: updatedArr,
+  });
+};
+
+module.exports = {
+  handleAllItems,
+  handleItemById,
+  handleCompanies,
+};
+// fs.writeFileSync("server/data/companies.json", JSON.stringify(updatedArr));
+// fs.writeFileSync("server/data/items.json", JSON.stringify(updatedItemArr));
--- server/index.js
+++ server/index.js
@@ -1,30 +1,42 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+// thira
+const {
+  handleAllItems,
+  handleItemById,
+  handleCompanies,
+} = require("./handlers");
 
 const PORT = 4000;
 
 express()
-  .use(function(req, res, next) {
+  .use(function (req, res, next) {
     res.header(
-      'Access-Control-Allow-Methods',
-      'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+      "Access-Control-Allow-Methods",
+      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
     );
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('./server/assets'))
+  .use(morgan("tiny"))
+  .use(express.static("./server/assets"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-  .use('/', express.static(__dirname + '/'))
+  .use("/", express.static(__dirname + "/"))
 
   // REST endpoints?
-  .get('/bacon', (req, res) => res.status(200).json('🥓'))
+  .get("/bacon", (req, res) => res.status(200).json("🥓"))
+
+  // thira's get endpoint
+  .get("/allitems", handleAllItems)
+  .get("/item/:id", handleItemById)
+  .get("/companies", handleCompanies)
 
   .listen(PORT, () => console.info(`Listening on port ${PORT}`));
