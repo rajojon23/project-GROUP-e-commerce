93cd884e267a132a08d560234397170baa9bb902
rajojon23 <rajojon23@hotmail.com>
2020-11-20 18:06:58 -0500

    adding redux dependency

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { Provider } from 'react-redux';
 import App from "./components/App";
+
 
 ReactDOM.render(
   <React.StrictMode>
