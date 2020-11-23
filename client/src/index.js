9a5d9a8e19529237302b71284a414be7709aab0b
rajojon23 <rajojon23@hotmail.com>
2020-11-23 10:47:04 -0500

    adding redux logic

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { createStore } from 'redux';
 import { Provider } from "react-redux";
+import reducer from './reducers';
 import App from "./components/App";
 
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
