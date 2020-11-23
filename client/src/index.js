6e0667110da80c74581fe5c3cf74cf9186c2a1d2
marcospoto <marcospoto@hotmail.com>
2020-11-23 10:34:37 -0500

    pagination

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import App from "./components/App";
-
 
 ReactDOM.render(
   <React.StrictMode>
