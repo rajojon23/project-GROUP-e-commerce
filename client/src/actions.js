bd179c1048c84e96d9b40fc88299186bd2c4e583
Tantely Rabefarihy <tantelyrab@gmail.com>
2020-11-24 09:56:51 -0500

    filtering

--- client/src/actions.js
+++ client/src/actions.js
@@ -1,11 +1,26 @@
-export const addItem = item => (  
-    {
-    type: 'ADD_ITEM',
-    item,
-  });
-  
-  export const removeItem = item => ({
-      type: 'REMOVE_ITEM',
-      item,
-  });
-  
+export const addItem = (item) => ({
+  type: "ADD_ITEM",
+  item,
+});
+
+export const removeItem = (item) => ({
+  type: "REMOVE_ITEM",
+  item,
+});
+
+// creating some actions in order to filter the data based on our choice
+
+export const filterByPrice = (payload) => ({
+  type: "FILTER_BY_PRICE",
+  payload,
+});
+
+export const filterByCategory = (payload) => ({
+  type: "FILTER_BY_CATEGORY",
+  payload,
+});
+
+export const filterByBrand = (payload) => ({
+  type: "FILTER_BY_BRAND",
+  payload,
+});
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,35 +1,43 @@
-import React from 'react';
-
+import React from "react";
 
 const initialState = {};
 
 export default function cartReducer(state = initialState, action) {
   switch (action.type) {
-        case 'ADD_ITEM': {
-            
-             return {
-               ...state,
-               [action.item.id]: {
-                 ...action.item,
-                 quantity: 1,
-               }
-             }
-           }
-        case 'REMOVE_ITEM': {
+    case "ADD_ITEM": {
+      return {
+        ...state,
+        [action.item.id]: {
+          ...action.item,
+          quantity: 1,
+        },
+      };
+    }
+    case "REMOVE_ITEM": {
+      const stateCopy = { ...state }; // New object we CAN mutate
+      delete stateCopy[action.item.id];
 
-            const stateCopy = { ...state }; // New object we CAN mutate
-            delete stateCopy[action.item.id];
+      return {
+        ...stateCopy,
+      };
+    }
 
-            return {
-              ...stateCopy,
-              
-            }
-          }
-
-        default:
-            return state;        
-        
+    default:
+      return state;
   }
 }
 
 export const getStoreItemArray = (state) => Object.values(state);
+
+export const filterStore = (state = initialState, action) => {
+  switch (action.type) {
+    case "FILTER_BY_PRICE":
+      return state;
+    case "FILTER_BY_CATEGORY":
+      return state;
+    case "FILTER_BY_BRAND":
+      return state;
+    default:
+      return state;
+  }
+};
